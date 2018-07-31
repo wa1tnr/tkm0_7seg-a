c797fc0fb7341e5be0937dfcbb4e9ec97bc4df36
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-31 01:16:42 +0000

    modified:   atmel_start_pins.h 	modified:   gpio_local.c 	modified:   gpio_local.h 	modified:   main.c

--- AtmelStart.d/atst_D51_aa_rev00d/main.c
+++ AtmelStart.d/atst_D51_aa_rev00d/main.c
@@ -1,5 +1,9 @@
 #include <atmel_start.h>
+#include "driver_examples.h"
+#include "driver_init.h"
+#include "utils.h"
 #include "gpio_local.h"
+
 
 /* Many changes: by wa1tnr, July 2018 */
 
@@ -18,13 +22,16 @@ void short_timer(void) { // human blinkie timescale
 
 void blink_LED(void) {
     raise_D13();
+    raise_D12();
     short_timer(); // should be first instance of use of this timer
     lower_D13();
+    // lower_D12();
     short_timer();
 }
 
 void flicker_LED(void) {
     raise_D13();
+    raise_D12();
     short_timer();
     lower_D13();
     short_timer();
@@ -41,11 +48,22 @@ void activity_LED_demo(void) {
     blinkLEDfast();
 }
 
+void long_long_timer(void) {
+    for (int j=201444; j>1; j--){
+        for (int i=201444; i>1; i--){
+            short_timer();
+        } // fake delay
+    } // faker delay haha
+}
+
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
-        raise_D13();
+        raise_D13(); // LED ON - why not?
+        raise_D12(); // metro m4 kludged
+
+        long_long_timer();
 
         // activity_LED_demo();
 
