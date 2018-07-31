299000243249868e25bd3588f528bb8d752a3cf3
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-31 14:08:54 +0000

    New names for these. 	modified:   gpio_local.h

--- AtmelStart.d/atst_D51_aa_rev00g3/gpio_local.h
+++ AtmelStart.d/atst_D51_aa_rev00g3/gpio_local.h
@@ -1,5 +1,10 @@
 extern void init_act_LED(void);
-extern void raise_D13(void);
-extern void raise_D12(void);
-extern void lower_D13(void);
-extern void lower_D12(void);
+// extern void raise_D13(void);
+// extern void raise_D12(void);
+// extern void lower_D13(void);
+// extern void lower_D12(void);
+
+extern void raise_D13-metro();   // Metro M4 Express
+extern void raise_D13-feather(); // Feather M4 Express
+extern void lower_D13-metro();
+extern void lower_D13-feather();
